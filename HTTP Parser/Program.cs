ec7d31b55445b7c7c29562597dc7b37de20c5396
Animesh Jain <Anjain@tavisca.com>
2019-07-30 23:57:00 +0530

    Added HTTP Request Parser

---
+++ HTTP Parser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var RequestDictionary = Parse("RequestHeader.txt");
+            foreach (var entry in RequestDictionary)
+            {
+                if (entry.Value != "")
+                    Console.WriteLine($"{entry.Key}) --> {entry.Value}");
+                else
+                    Console.WriteLine($"{entry.Key}) --> No {entry.Value}");
+            }
+        }
+    }
+}
---
+++ HTTP Parser/ResponseParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpParser
+{
+
+    class HttpResponseParser
+    {
+
+        public static Dictionary<string, string> Parse(string filename)
+        {
+            var temp = new Dictionary<string, string>();
+            using (StreamReader file = new StreamReader(filename))
+            {
+                string ln = file.ReadLine();
+                string[] s = ln.Split(new[] { ' ' }, 2);
+                temp.Add("HttpAndVersion", s[0]);
+                temp.Add("StatusCode", s[1]);
+
+                while ((ln = file.ReadLine()) != null)
+                {
+
+                    if (ln.Contains(":"))
+                    {
+                        string[] sp = ln.Split(new[] { ':' }, 2);
+                        temp.Add(sp[0], sp[1]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+
+                string data = file.ReadToEnd();
+                temp.Add("Payload", data);
+                file.Close();
+
+            }
+
+            return temp;
+        }
+
+
+
+    }
+}
